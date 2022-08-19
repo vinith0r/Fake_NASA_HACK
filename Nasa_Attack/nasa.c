649a7db5ab89fa35dccb952e285b7b2cf979ca91
vinith <111608591+vinith0r@users.noreply.github.com>
2022-08-20 03:05:12 +0530

    Add files via upload

---
+++ Nasa_Attack/nasa.c
@@ -0,0 +1,242 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+	int a,b,c;
+        int age;
+        printf("enter your age : ");
+        scanf(" %d", &age);
+
+        if (age <= 17)
+        {
+                printf("you cannnot accsess this tool\n\n");
+		exit(0);
+        }
+	if (age >= 100)
+	{
+		printf("\n\ninvalid option !");
+	}
+
+        if (age == 18)
+        {
+                printf("wait one more year to accsess this tool\n");
+			exit(0);
+        }
+        if (age >= 19)
+        {
+		printf("you can access wait a munite it takes time......../.\n");
+
+		for (age = 0; age <= 100000; age++)
+                {
+			printf("..................../0%%\n.................../12%%\n................./19%%\n................/27%%\n.............../39%%\n............../51%%\n............./72%%\n............/86%%\n.........../93%%\n........../100%%\n");
+		}
+        }
+	printf ("#do you want to hack a nasa first you need to run a VPN \nremember without vpn is danger for you\n");
+	printf ("turn on proxy vpn via this tool put (17)\n");
+	int hello;
+	printf("#enter : ");
+	scanf("%d", &hello);
+	if (hello = 17)
+		printf("searching for vpn..............................................\n.....................................................................\n..................\n................................\n..................................\n........................................................................................\n..........................................\n................................................\n..............................................\n........................................................... vpn connected.\n ");
+	else 
+		printf("any way without vpn is danger for remmember this\n\n");
+	printf ("#do you want to continue just put (1) and hit enter \n ");
+	printf ("#enter 2 to get about creator\n");
+	printf ("#choose : ");
+	scanf("%d", &a);
+	{
+		if (a == 1)
+		{
+			printf ("enter 2 to hack nasa : ");
+			scanf("%d", &b);
+			{
+				if (b == 2)
+				{
+					for (b = 0; b <= 100000; b++)
+					{
+						printf ("hacking nasa 0%%");
+						printf ("hacking nasa 1%%");
+						printf ("hacking nasa 20%%");
+						printf ("hacking nasa 26%%");
+						printf ("hacking nasa 37%%");
+						printf ("hacking nasa 42%%");
+						printf ("hacking nasa 57%%");
+						printf ("hacking nasa 66%%");
+						printf ("hacking nasa 78%%");
+						printf ("hacking nasa 89%%");
+						printf ("hacking nasa 94%%");
+						printf ("hacking nasa 100%%\n");
+					}
+		
+						printf ("succsess fully haked a nasa..\n now turn off your computer for your safty..\n");
+						printf ("do you want to get about creaator put 2");
+						scanf("%d", &a);
+		if (a == 2)
+		{
+			printf ("\n\n# created by vinith\n\n# email : vinith87vr@gmail.com\n\n#mobile : 9739168001 \n\n");
+		}
+		else
+		{
+			printf ("put correct option try again \n");
+		}
+							printf("do you want to get some websites for learn hacking and programming \n just put 1...\n and  do you want to exit put 0\n");
+							int vinith;
+		printf("#enter : ");
+		scanf("%d", &vinith);
+		{
+			if (vinith == 1)
+			{
+				int vnith;
+				printf("(1) haking wesites\n");
+				printf("(2) programing \n");
+				printf("(3) exit");
+				printf("#choose : ");
+				scanf("%d", &vnith);
+				{
+					if (vnith == 1)
+					{
+						printf("# hackerone.com\n # hackthebox.com\n # stackowerflow.com\n # linuxjourny.com\n");
+					}
+					if (vnith == 2)
+					{
+						printf("# sololearn.com \n # w3schools.com\n # beginnerbooks.com\n # learnxinymunites.com \n # linuxjourny.com \n ");
+					}
+					if (vnith == 3)
+					{
+						printf("exiting.....");
+						exit(0);
+					}
+					else 
+					{
+						printf("put correct option\n");
+					}
+
+				}
+			}
+
+			else
+			{
+				printf ("exiting.....\n");
+				exit(0);
+			}
+
+		}
+
+				}
+					else 
+					{
+						printf ("as well as do you want to exit put 3 : ");
+
+						scanf("%d", &c);
+						{
+							if (c == 3)
+							{
+								printf ("exiting......\n");
+								exit(0);
+							}
+						}
+					}
+				}
+			}
+		}
+		if (a == 2)
+		{
+			printf ("\n\n# created by vinith\n# email : vini01.me@gmail.com \n\n");
+		}
+		else
+		{
+			printf ("\nput correct option try again \n");
+		}
+
+		int vinith;
+		printf("\ndo you want to get some websites for learn hacking and programming \n just put 1...\n and  do you want to exit put 0\n");
+		printf("\n#enter : ");
+		scanf("%d", &vinith);
+		{
+			if (vinith == 1)
+			{
+				int vnith;
+				printf("\n(1) haking wesites\n");
+				printf("\n(2) programing \n");
+				printf("\n(3) exit");
+				printf("\n#choose : ");
+				scanf("\n%d", &vnith);
+				{
+					if (vnith == 1)
+					{
+						printf("# hackerone.com\n # hackthebox.com\n stackowerflow.com\n # linuxjourny.com");
+					}
+					if (vnith == 2)
+					{
+						printf("# sololearn.com \n # w3schools.com\n # beginnerbooks.com\n # learnxinymunites.com \n # linuxjourny.com \n ");
+					}
+					if (vnith == 3)
+					{
+						printf("exiting.....");
+						exit(0);
+					}
+
+				}
+			}
+
+			else
+			{
+				printf ("exiting.....\n");
+				exit(0);
+			}
+
+		}
+		char gaint[100];
+
+		printf("enter your (u)name : ");
+		scanf("%s", gaint);
+		printf("welcome %s\n\n", gaint);
+		int hr;
+		printf("(1) hack your friends\n");
+		printf("(2) attak friend gmail account\n");
+		printf("(3) exit\n");
+		printf("# choose : ");
+		scanf ("%d", &hr);
+		{
+			if (hr == 1)
+			{
+				int fr;
+				printf("(1) friend mobile\n");
+				printf("(2) friend desktop\n");
+				printf("# enter : ");
+				scanf("%d", &fr);
+				{
+					int mo;
+					printf("enter your freiend mobile number : ");
+					scanf("%d", &mo);
+					{
+						for (mo = 0; mo <= 100000; mo++)
+						printf("attacking .......\n");
+					}
+					printf("succsess fully attacked a mobile\n");
+				}
+			}
+			if (hr == 2);
+			{
+				int gm;
+				printf("enter gmail id : ");
+				scanf("%d", &gm);
+				{
+					for (gm = 0; gm <= 100000; gm++ )
+					{
+						printf("attacking gmail.......");
+					}
+					printf("succsess fully attacked a gmail account \n");
+				}
+			}
+			if (hr == 3)
+			{
+				printf("exiting........\n");
+				exit(0);
+			}
+
+		}
+
+}
+
